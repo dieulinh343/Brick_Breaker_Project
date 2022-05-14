e5d1ad4813dee934e15b27723b82af532afc4a63
lechicuong2k3 <105204420+lechicuong2k3@users.noreply.github.com>
2022-05-14 12:52:02 +0700

    Update GamePanel.java

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -20,11 +20,16 @@ public class GamePanel extends JPanel implements Runnable {
 	private Graphics2D g;
 	private MouseListener MouseListener;
 	private int mouseX;
+	int paddleWidth = 75;
+	int ballSize = 15; 
+	int powerSize1=20, powerSize2 =20;
 	
+	PowerUps powerup1, powerup2, powerup3; //them
 	Ball ball;
 	Paddle paddle;
 	Map map;
 	HUD HUD;
+	int count1, count2, count3, count4, count5 =0;
 	
 	public GamePanel() {
 		init();
@@ -32,12 +37,15 @@ public class GamePanel extends JPanel implements Runnable {
 	
 	public void init() {
 		mouseX = 0;
-		ball = new Ball();
-		paddle = new Paddle();
+		ball = new Ball(ballSize);
+		paddle = new Paddle(paddleWidth);
 		map = new Map(6, 10);
 		HUD = new HUD();
+		powerup1 = new PowerUps(190, 75, powerSize1, "powerUp1.png");
+		powerup2 = new PowerUps(287, 166, powerSize2, "powerUp4.png");//them
 		MouseListener = new MouseListener();
 		addMouseMotionListener(MouseListener);
+	
 		
 		running = true;
 		image = new BufferedImage(MainMenu.WIDTH, MainMenu.HEIGHT, BufferedImage.TYPE_INT_RGB);
@@ -64,6 +72,8 @@ public class GamePanel extends JPanel implements Runnable {
 	}
 	
 	public void checkCollisions() {
+		Rectangle powerupRect1 = powerup1.getRect();
+		Rectangle powerupRect2 = powerup2.getRect();
 		Rectangle ballRect = ball.getRect();
 		Rectangle paddleRect = paddle.getRect();
 		
@@ -74,6 +84,24 @@ public class GamePanel extends JPanel implements Runnable {
 			}
 			if(ball.getX() < mouseX + paddle.getWidth() && ball.getX() > mouseX + paddle.getWidth()/4) {
 				ball.setDX(ball.getDX() + 0.5);
+			}
+		}
+		
+		if (powerupRect1.intersects(paddleRect) && count1 < 2) {
+			paddleWidth += 30;
+			paddle = new Paddle(paddleWidth);
+			count1 +=1;
+			if (count1 ==1) {
+				powerup1.delPower();
+			}
+		}
+		
+		if (powerupRect2.intersects(paddleRect) && count2 < 2) {
+			ballSize += 10;
+			ball = new Ball(ballSize);
+			count2 +=1;
+			if (count2 ==1) {
+				powerup2.delPower();
 			}
 		}
 		
@@ -88,13 +116,31 @@ public class GamePanel extends JPanel implements Runnable {
 					int height = map.getBrickHeight();
 					
 					Rectangle brickRect = new Rectangle(brickx, bricky, width, height); 
-					
 					if (ballRect.intersects(brickRect)) {
+						if (row == 1 && col == 2 && count3 == 0) {
+							map.hitBrick(row, col);
+							ball.setDY(-ball.getDY());
+							HUD.addScore();
+							count3+=1;
+							powerSize1=35;
+							powerup1 = new PowerUps(190, 75, powerSize1, "powerUp1.png");
+						}
+						else if (row == 5 && col == 4 && count4 == 0){
+							
+							map.hitBrick(row, col);
+							ball.setDY(-ball.getDY());
+							HUD.addScore();
+							count4 +=1;
+							powerSize2 = 35;
+							powerup2 = new PowerUps(287, 166, powerSize2, "powerUp4.png");
+						}
+						else {
 						map.hitBrick(row, col);
 						ball.setDY(-ball.getDY());
 						HUD.addScore();
 						
 						break A;
+					}
 					}
 				}
 			}
@@ -103,7 +149,16 @@ public class GamePanel extends JPanel implements Runnable {
 	
 	public void update() {
 		checkCollisions();
-		ball.update();
+		if (count5 <100) {count5+=1;
+		}
+		else{ball.update();}
+		if (powerSize1 == 35) {
+			powerup1.move();
+		}
+		if (powerSize2 == 35) {
+			powerup2.move();
+		}
+		
 	}
 	
 	public void draw() {
@@ -115,26 +170,38 @@ public class GamePanel extends JPanel implements Runnable {
 		paddle.draw(g);
 		map.draw(g);
 		HUD.draw(g);
+		powerup1.draw(g, this);
+		powerup2.draw(g, this);
 		
 		if (map.winCheck() == true) {
 			drawWin();
+			
 		}
 		if (ball.getLose() == true) {
 			drawLose();
+			
 		}
 	}
 	
 	public void drawWin() {
+		running = false;
 		g.setColor(Color.RED);
 		g.setFont(new Font("serif", Font.BOLD, 30));
-		g.drawString("You Win!", 240, 250);
+		g.drawString("You Win!", 255, 250);
+		
+		g.setFont(new Font("serif",Font.BOLD, 20));
+		g.drawString("Press Enter to Restart", 220, 270);
 	}
 	
 	public void drawLose() {
+		running = false;
 		g.setColor(Color.RED);
 		g.setFont(new Font("serif", Font.BOLD, 30));
 		g.drawString("You Lose", 255, 250);
 		g.drawString("Score: " + HUD.getScore(), 255, 275);
+		
+		g.setFont(new Font("serif",Font.BOLD, 20));
+		g.drawString("Press Enter to Restart", 220, 295);
 	}
 	
 	public void paintComponent(Graphics g) {
@@ -157,5 +224,11 @@ public class GamePanel extends JPanel implements Runnable {
 			paddle.mouseMoved(e.getX()-50);
 		}
 	}
-}
+	
+
+
+		
+			
+		
+	}
 	
